4f2b51989cd772c003ed0f84c159e92f462199df
Matthew Lucas <mlucas24@icloud.com>
2019-12-05 20:20:14 -0500

    connected mongoose to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+let express = require("express");
+let bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+const password = require("./password");
+
+mongoose
+  .connect(
+    `mongodb+srv://mattyshiloh23:${password}@gammazon-topbar-2wyhz.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "Gammazon"
+    }
+  )
+  .then(() => {
+    console.log("connected to mongo!");
+  });
+var db = mongoose.connection;
 
 var app = express();
 app.use(express.json());
