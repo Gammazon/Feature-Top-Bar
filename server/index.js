bb0f8295ef57feb9fe53e460197d0d35ca1b5621
Matthew Lucas <mlucas24@icloud.com>
2019-12-10 15:06:31 -0600

    Created Routes for the Menu

--- server/index.js
+++ server/index.js
@@ -2,21 +2,9 @@ let express = require("express");
 let bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 const password = require("./password");
-
-mongoose
-  .connect(
-    `mongodb+srv://mattyshiloh23:${password}@gammazon-topbar-2wyhz.mongodb.net/test?retryWrites=true&w=majority`,
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: "Gammazon"
-    }
-  )
-  .then(() => {
-    console.log("connected to mongo!");
-  });
-var db = mongoose.connection;
+const db = require("../db/index");
+const Menu = require("../db/Menu");
+const cors = require("cors");
 
 var app = express();
 app.use(express.json());
@@ -25,6 +13,17 @@ app.use(bodyParser.json());
 
 app.use(express.static("dist"));
 
+app.get("/all", async (req, res) => {
+  const menu = await Menu.find({});
+
+  try {
+    res.send(menu);
+  } catch (err) {
+    res.status(502).send(err);
+  }
+});
+
 app.listen(2424, function() {
   console.log("listening on port 2424!");
 });
+module.exports = app;
